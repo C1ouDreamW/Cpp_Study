cc2c210a2adb6f4079114d5f5a58d18b1147fc67
C1ouD <cloud_aaa@163.com>
2025-01-24 18:33:14 +0800

    Dayliy

--- VS project/Pace_Now/NC_2/NC_2/main.cpp
+++ VS project/Pace_Now/NC_2/NC_2/main.cpp
@@ -1,16 +1,13 @@
 //牛客算法竞赛第二场
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long
 
 void solve() {
 	
 }
 
-signed main() {
-	int t; cin >> t;
-	while (t--) {
-		solve();
-	}
+
+int main() {
+	solve();
 	return 0;
 }
